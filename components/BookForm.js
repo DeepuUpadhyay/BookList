45947f79ef480936bef417be0c9e4ca27fd17736
DeepuUpadhyay <61281750+DeepuUpadhyay@users.noreply.github.com>
2020-02-28 14:04:02 +0530

    Initial Draft

---
+++ components/BookForm.js
@@ -0,0 +1,36 @@
+import React, { useContext, useState } from "react";
+import { BookContext } from "../contexts/BookContext";
+
+const BookForm = () => {
+ const { dispatch } = useContext(BookContext);
+ const [title, setTitle] = useState("");
+ const [author, setAuther] = useState("");
+
+ const handleSubmit = e => {
+  e.preventDefault();
+  dispatch({ type: "ADD_BOOK", book: { title, author } });
+  setTitle("");
+  setAuther("");
+ };
+ return (
+  <form onSubmit={handleSubmit}>
+   <input
+    type="text"
+    placeholder=" book title"
+    value={title}
+    onChange={e => setTitle(e.target.value)}
+    required
+   />
+   <input
+    type="text"
+    placeholder=" book author"
+    value={author}
+    onChange={e => setAuther(e.target.value)}
+    required
+   />
+   <input type="submit" value="add book" />
+  </form>
+ );
+};
+
+export default BookForm;
